22632c73344dc885b95c84d1362473dce31908d3
Mars190 <machstoehr@gmail.com>
2023-08-02 11:10:34 +0200

    Main renamed to Bubblesort

--- src/Bubblesort.java
+++ src/Bubblesort.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Main {
+public class Bubblesort {
     public static int[] list;
 
     public static void main(String[] args) {
--- src/BubblesortTest.java
+++ src/BubblesortTest.java
@@ -1,13 +1,13 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class MainTest {
+public class BubblesortTest {
 
     @Test
     public void testSort() {
         int[] input = {2, 9, 3, 2, 0, 5, 2, 0, 8, 1};
         int[] expectedOutput = {0, 0, 1, 2, 2, 2, 3, 5, 8, 9};
-        int[] output = Main.sort(input);
+        int[] output = Bubblesort.sort(input);
 
         assertArrayEquals(expectedOutput, output);
     }
